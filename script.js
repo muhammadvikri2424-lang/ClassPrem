5a67ac79ffdc7dac349e06ed2c26dfbdbdf30176
muhammadvikri2424-lang <muhammadvikri2424@gmail.com>
2026-08-12 15:59:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,56 @@
 // CLASSUP WEBSITE SCRIPT
 
 
-// Animasi saat halaman dibuka
-
-document.addEventListener("DOMContentLoaded", function(){
-
-    console.log("ClassUp Website Ready");
-
-});
-    
-
-
 // Efek scroll navbar
-
 window.addEventListener("scroll", function(){
 
-    const header = document.querySelector("header");
+const header = document.querySelector("header");
 
+if(window.scrollY > 50){
 
-    if(window.scrollY > 50){
+header.style.boxShadow =
+"0 5px 25px rgba(0,0,0,0.15)";
 
-        header.style.boxShadow =
-        "0 5px 25px rgba(0,0,0,0.15)";
+}else{
 
-    }else{
+header.style.boxShadow =
+"0 5px 20px rgba(0,0,0,0.08)";
 
-        header.style.boxShadow =
-        "0 5px 20px rgba(0,0,0,0.08)";
-
-    }
-
+}
 
 });
-
-
 
 
 // Animasi card program
-
 const cards = document.querySelectorAll(".card");
-
 
 cards.forEach(card=>{
 
+card.addEventListener("mouseenter",()=>{
 
-    card.addEventListener("mouseenter",()=>{
+card.style.transform =
+"translateY(-15px)";
 
-        card.style.transform =
-        "translateY(-15px)";
-
-    });
-
-
-
-    card.addEventListener("mouseleave",()=>{
-    card.style.transform =
-    "translateY(0)";
 });
 
-});   // TAMBAHKAN INI
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform =
+"translateY(0)";
+
+});
+
+});
 
 
+// Tombol daftar WhatsApp
 function daftarClassUp(){
 
 let nama = document.getElementById("nama").value;
 let usia = document.getElementById("usia").value;
 let program = document.getElementById("program").value;
+
 
 let pesan =
 "Halo ClassUp, saya ingin mendaftar kursus.%0A%0A" +
